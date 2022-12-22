6fd2801492650fc7efe127c0b46476edd77dfeec
IanTaiAhn <iantaiahn@yahoo.com>
2022-12-22 15:47:54 -0700

    api call functionality currently being added.

--- vue-project/src/components/NavbarItem.vue
+++ vue-project/src/components/NavbarItem.vue
@@ -13,8 +13,8 @@ defineProps({
 
 
 <template>
-    <a :href=route class="hover:text-gray-400 group relative">
+    <a :href=route class="hover:text-gray-300 group relative">
         {{ msg }}
-        <span class="bg-yellow-300 absolute left-0 -bottom-1 w-0 h-1 transition-all group-hover:w-full"></span>
+        <span class="bg-black absolute left-0 -bottom-1 w-0 h-1 transition-all group-hover:w-full"></span>
     </a>
 </template>
--- vue-project/src/components/TheProduct.vue
+++ vue-project/src/components/TheProduct.vue
@@ -1,21 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      price: 0
+    }
+  },
+  // `mounted` is a lifecycle hook which we will explain later
+  mounted() {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then((json=> {
+                console.log(json);
+                this.title = json.title;
+                this.price = json.price;
+            }))
+  }
+}
+</script>
 <script setup>
     import ProductItem from './ProductItem.vue';
     import TheImage from './TheImage.vue';
-
-    fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
 </script>
 
 <template>
     <div class="grid grid-col sm:grid sm:grid-cols-3 justify-items-center border border-black p-2 space-y-4">
-        <!-- Instead of creating product items individually, figure out how to loop through them in accoradance to the api calls. -->
         <ProductItem class="mt-4">
             <template #image>
                 <TheImage/>
             </template>
-            <template #title>test title</template>
-            <template #price>test price</template>
+            <template #title>{{ title }}</template>
+            <template #price>${{ price }}</template>
         </ProductItem>
 
         <ProductItem/>
