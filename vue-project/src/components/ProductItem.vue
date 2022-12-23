7d788cace69397c9133358cb635030c14a056983
IanTaiAhn <iantaiahn@yahoo.com>
2022-12-22 17:46:13 -0700

    Images are working. Next is the looping functionality

--- vue-project/src/components/ProductItem.vue
+++ vue-project/src/components/ProductItem.vue
@@ -1,11 +1,19 @@
 <script setup>
-
+defineProps({
+  childImg: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
 <template>
     <div class="w-64 h-72 border border-black p-2">
         <div class="flex flex-col justify-between h-full p-2">
-            <p class="self-center"><slot name="image">null image</slot></p>
+            <!-- <p class="self-center"><slot name="image">null image</slot></p> -->
+            <div class=" p-2">
+                <img :src=childImg alt="test" class=""> 
+            </div>
             <div class="flex flex-row justify-between">
                 <h1 class=""><slot name="title">null title</slot></h1> 
                 <h1 class=""><slot name="price">null price</slot></h1> 
--- vue-project/src/components/TheProduct.vue
+++ vue-project/src/components/TheProduct.vue
@@ -3,40 +3,50 @@ export default {
   data() {
     return {
       title: "",
-      price: 0
+      price: 0,
+      titleArr: ["title1", "title2", "title3", "title4", "title5"],
+      priceArr: ["price1", "price2", "price3", "price4", "price5"],
+      imgURL: "",
+      img: {}
     }
   },
   // `mounted` is a lifecycle hook which we will explain later
   mounted() {
-    fetch('https://fakestoreapi.com/products')
+    // fetch('https://fakestoreapi.com/products/1')
+    //         .then((res=>{
+    //             res
+    //         }))
+    fetch('https://fakestoreapi.com/products/1')
             .then(res=>res.json())
             .then((json=> {
                 console.log(json);
                 this.title = json.title;
                 this.price = json.price;
+                this.imgURL = json.image;
+                console.log(this.imgURL);
             }))
   }
+
 }
 </script>
 <script setup>
     import ProductItem from './ProductItem.vue';
     import TheImage from './TheImage.vue';
+    let testString = "hooby";
 </script>
 
 <template>
     <div class="grid grid-col sm:grid sm:grid-cols-3 justify-items-center border border-black p-2 space-y-4">
-        <ProductItem class="mt-4">
-            <template #image>
-                <TheImage/>
-            </template>
+        <ProductItem class="mt-4" :childImg="imgURL">
             <template #title>{{ title }}</template>
             <template #price>${{ price }}</template>
         </ProductItem>
-
         <ProductItem/>
         <ProductItem/>
         <ProductItem/>
         <ProductItem/>
-        
+        <!-- <img src={{ img }} alt=""> -->
+        <!-- Ideally I loop through the product items, and we only fetch data here. -->
+        <!-- <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt=""> -->
     </div>
 </template>
